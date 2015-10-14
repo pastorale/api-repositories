fcbca0c66be6869b2669f423bbe0c6b75781e8dc
Lê Thanh Bình <binh@sunrise.vn>
2015-10-14 12:22:32 +0700

    back with uppercase JobBoard

---
+++ Organisation/PositionRepository.php
@@ -0,0 +1,23 @@
+<?php
+namespace AppBundle\Repositories\Organisation;
+
+use AppBundle\Entity\Organisation\Position;
+use Doctrine\ORM\EntityRepository;
+
+class PositionRepository extends EntityRepository
+{
+    /**
+     * @param $orgCode
+     * @param $userCode
+     * @return Position
+     */
+    public function findOneByVerificationCode($orgCode, $userCode)
+    {
+        $queryBuilder = $this->createQueryBuilder('p');
+        $position = $queryBuilder->join('p.employee', 'u', 'WITH', 'u.code = ?1')
+            ->join('p.employer', 'o', 'WITH', 'o.code = ?2')
+            ->where($queryBuilder->expr()->like('p.active', '?3'))
+            ->setParameters(array(1 => $userCode, 2 => $orgCode, 3 => true));
+        return $position->getQuery()->setMaxResults(1)->getOneOrNullResult();
+    }
+}
