aeb24b66b60c0334e433b5212ae73cbf7089035a
Lê Thanh Bình <binh@sunrise.vn>
2016-04-13 22:19:26 +0700

    - Mapped CandidateReviewer - JobBoard - filter by CandiateReviewer
    TODO:
    - Map  CandidateFolder
    - Implement JobCandidateRepository
    - Filter the Candidate by CandidateFolder in JobBoardRetrievalController

--- Organisation/PositionRepository.php
+++ Organisation/PositionRepository.php
@@ -1,11 +1,33 @@
 <?php
 namespace AppBundle\Repositories\Organisation;
 
+use AppBundle\Entity\Core\User\User;
+use AppBundle\Entity\Organisation\Organisation;
 use AppBundle\Entity\Organisation\Position;
+use Doctrine\Common\Collections\Criteria;
 use Doctrine\ORM\EntityRepository;
 
 class PositionRepository extends EntityRepository
 {
+    /**
+     * @param User $user
+     * @param Organisation $organisation
+     * @param Criteria|null $criteria
+     * @return Position|null
+     */
+    public function findOneByEmployer(User $user, Organisation $organisation, Criteria $criteria = null)
+    {
+        if ($criteria === null) {
+            $criteria = Criteria::create();
+        }
+        $criteria
+            ->andWhere(Criteria::expr()->eq("employer", $organisation))
+            ->setFirstResult(0)
+            ->setMaxResults(1);
+        $positions = $user->getPositions()->matching($criteria);
+        return $positions->get(0);
+    }
+
     /**
      * @param $orgCode
      * @param $userCode
