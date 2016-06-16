4079c492e84327e02638f6cc60aad6c26de26896
Chuyen Nguyen <chuyen.nguyen@synova.com.vn>
2016-06-16 13:39:35 +0700

    fix issue about authorize user

--- Core/Core/UserGroupACERepository.php
+++ Core/Core/UserGroupACERepository.php
@@ -3,6 +3,7 @@ namespace AppBundle\Repositories\Core\Core;
 
 use AppBundle\Entity\Organisation\Position;
 use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\EntityRepository;
 
 class UserGroupACERepository extends EntityRepository
@@ -13,17 +14,21 @@ class UserGroupACERepository extends EntityRepository
      * @return Position
      * // TODO implementation
      */
-    public function findByAttributeGroups($attribute, ArrayCollection $groups, $childClassname = null)
+    public function findByAttributeGroups($attribute, Collection $groups, $childClassname = null)
     {
         $qb = $this->createQueryBuilder('ace');
         $expr = $qb->expr();
-        $qb->where($expr->like('ace.attributes', '%[' . $attribute . ']%'))
-            ->andWhere($expr->in('ace.group', $groups));
 
+        $qb->where($expr->like('ace.attributes', ':attribute'))
+            ;
+        if($groups->count()>0) {
+            $qb->andWhere( $expr->in( 'ace.group', $groups->getValues() ) );
+        }
         if ($childClassname !== null) {
             $qb->andWhere('ace INSTANCE OF :childClassName')
                 ->setParameter(':childClassName', $childClassname);
         }
+        $qb->setParameter('attribute','%[' . $attribute . ']%');
         return $qb->getQuery()->setMaxResults(1)->getOneOrNullResult();
 //        return null;
     }
