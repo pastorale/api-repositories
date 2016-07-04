7a3816988737fe47cd09d43ec3c3292279ef84de
mikedutuandu <mikedutuandu@gmail.com>
2016-07-04 17:31:56 +0700

    update code

---
+++ Core/Core/UserACERepository.php
@@ -0,0 +1,19 @@
+<?php
+namespace AppBundle\Repositories\Core\Core;
+
+use AppBundle\Entity\Organisation\Position;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\EntityRepository;
+
+class UserACERepository extends EntityRepository
+{
+    /**
+     * @param $user
+     * @param $childClassname
+     */
+    public function findByAttributeUser($user, $childClassname = null)
+    {
+        //return HandbookUserACE
+    }
+}
