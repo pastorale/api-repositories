ad728eac4eb5fd78c7907d079a354413cd3b6235
Chuyen Nguyen <chuyen.nguyen@synova.com.vn>
2016-07-07 12:24:48 +0700

    update code

--- Core/Core/UserACERepository.php
+++ Core/Core/UserACERepository.php
@@ -9,11 +9,31 @@ use Doctrine\ORM\EntityRepository;
 class UserACERepository extends EntityRepository
 {
     /**
-     * @param $user
-     * @param $childClassname
+     * @param      $attributes
+     * @param      $user
+     * @param null $childClassname
+     * @param      $allowed
+     *
+     * @return mixed
+     * @throws \Doctrine\ORM\NonUniqueResultException
      */
-    public function findByAttributeUser($user, $childClassname = null,$allowed)
+    public function findByAttributeUser($attributes ,$user, $childClassname = null,$allowed)
     {
         //return HandbookUserACE
+        $qb = $this->createQueryBuilder('ace');
+        $expr = $qb->expr();
+
+        $qb->where($expr->eq( 'ace.user', ':user' ));
+
+        $qb->andWhere( $expr->eq( 'ace.allowed', ':allowed' ) );
+        $qb->setParameter( 'user', $user);
+        $qb->setParameter('allowed', $allowed);
+
+        if ($childClassname !== null) {
+            $qb->andWhere('ace INSTANCE OF '.$childClassname);
+        }
+
+        $temp = $qb->getQuery()->getDQL();
+        return $qb->getQuery()->setMaxResults(1)->getOneOrNullResult();
     }
 }
