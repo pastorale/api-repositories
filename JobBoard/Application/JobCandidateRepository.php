c4263111290ccba04554bb71f7603bcac76c6dc2
Lê Thanh Bình <binh@sunrise.vn>
2016-07-28 00:01:05 +0700

    - logged_in_candidate
    TODO:
    - users/id/positions: POST, PUT, DELETE
    - HATEOAS: JobCandidate to retrieve folders along with folder_reviewers
    - Map  CandidateFolder
    - JobListing Form CRUD - salary
    - QBSecurity Trait

--- JobBoard/Application/JobCandidateRepository.php
+++ JobBoard/Application/JobCandidateRepository.php
@@ -1,12 +1,25 @@
 <?php
 namespace AppBundle\Repositories\JobBoard\Application;
 
+use AppBundle\Entity\JobBoard\Application\JobCandidate;
 use AppBundle\Entity\Organisation\Position;
 use AppBundle\Services\Core\Framework\BaseRepository;
+use Doctrine\Common\Collections\Criteria;
 
 class JobCandidateRepository extends BaseRepository
 {
-   
-
-
+    /**
+     * @param $code
+     * @return JobCandidate|null
+     */
+    public function findOneByInvitationCode($code)
+    {
+        $queryBuilder = $this->createQueryBuilder('c')->join('c.invitationCode', 'invitation_code');
+        $expr = $queryBuilder->expr();
+        $queryBuilder
+            ->where($expr->eq('c.enabled', true))
+            ->andWhere($expr->andX($expr->eq('invitation_code.code', $code), $expr->eq('invitation_code.enabled', true)));
+//        $sql = $position->getQuery()->getSQL();
+        return $queryBuilder->getQuery()->setMaxResults(1)->getOneOrNullResult();
+    }
 }
